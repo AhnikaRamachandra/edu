71cd07584c9a6ec85b5079dd70558239656f7c93
ahnika <98440824+AhnikaRamachandra@users.noreply.github.com>
2024-03-29 09:10:02 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'boi':[  "Bai Jamjuree", 'sans-serif']
+      },
+      colors:{
+        'green':'#26BAA4',
+      }
+    },
+  },
+  plugins: [],
+}
+
